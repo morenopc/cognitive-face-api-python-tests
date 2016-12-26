b0e57620fb57602ddecd883b35e9bd0815cb6934
Moreno Cunha <moreno.pinheiro@gmail.com>
2016-12-26 20:59:03 -0200

    add a return example comment

--- minimal-usage-demo.py
+++ minimal-usage-demo.py
@@ -7,4 +7,15 @@ CF.Key.set(KEY)
 img_url = ('http://www.blastr.com/sites/blastr/files/styles/width_1280/'
            'public/images/frodo-baggins-LOTR.jpg')
 result = CF.face.detect(img_url)
+
+# Return example
+# <type 'list'>
+# [
+#     {
+#         u'faceId': u'89a11710-7122-4706-812e-1005ae5ec559',
+#         u'faceRectangle': {
+#             u'width': 253, u'top': 37, u'height': 242, u'left': 136}
+#     }
+# ]
+
 print result
